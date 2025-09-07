988cf38e5d1f49d4941723244bf2ddef21a7ead8
Mohamed1442 <mohamedmagdy7428@gmail.com>
2025-09-07 19:10:49 +0300

    refactor(frontend): adding neccessary packages and configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,15 @@ export default {
         secondary: "#ffd391",
         error: "#D80027",
       },
+      keyframes: {
+        rotate: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
+      animation: {
+        rotate: "rotate 1s linear infinite",
+      },
     },
   },
   plugins: [],
