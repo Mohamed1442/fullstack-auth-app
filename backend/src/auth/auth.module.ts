34b6012526cc1e2025199bf29684b2e813e5ddd8
Mohamed1442 <mohamedmagdy7428@gmail.com>
2025-09-07 03:19:36 +0300

    feat(backend): creating the requeired endpoints for handling authentication

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { AuthController } from './controllers';
+import { AuthService } from './services';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RefreshToken, RefreshTokenSchema } from './schemas';
+import { AccessTokenStrategy } from './strategies';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { UsersModule } from 'src/users/users.module';
+import { RefreshTokenRepository } from './repositories';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        signOptions: {
+          expiresIn: configService.getOrThrow(
+            'JWT_ACCESS_TOKEN_EXPIRE_TIME_STRING',
+          ),
+        },
+        secret: configService.getOrThrow('JWT_ACCESS_TOKEN_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeature([
+      { name: RefreshToken.name, schema: RefreshTokenSchema },
+    ]),
+    UsersModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, RefreshTokenRepository, AccessTokenStrategy],
 })
 export class AuthModule {}
---
+++ backend/src/auth/controllers/auth.controller.ts
@@ -0,0 +1,99 @@
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import type { Request, Response } from 'express';
+import ms from 'ms';
+import { AccessTokenGuard } from 'src/global/guards';
+import { TransactionInterceptor } from 'src/global/interceptors';
+import { ResponseBuilder } from 'src/global/utilities';
+import { LoginRequestDto } from '../dtos/requests';
+import { SignupRequestDto } from '../dtos/requests/signup.request.dto';
+import { LoginResponseDto } from '../dtos/responses';
+import { AuthService } from '../services';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { ApiSingleDataResponse } from 'src/global/decorators';
+
+@Controller('auth')
+@UseInterceptors(TransactionInterceptor)
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Post('register')
+  @ApiOperation({ summary: 'Registeration for new user' })
+  @ApiSingleDataResponse(LoginResponseDto)
+  async register(@Body() signupRequestDto: SignupRequestDto) {
+    const tokens = await this.authService.register(signupRequestDto);
+
+    return ResponseBuilder.buildSingle(new LoginResponseDto(tokens));
+  }
+
+  @Post('login')
+  @ApiOperation({ summary: 'User login' })
+  @ApiSingleDataResponse(LoginResponseDto)
+  async login(
+    @Body() loginRequestDto: LoginRequestDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const tokens = await this.authService.login(loginRequestDto);
+
+    // set refresh token in cookie
+    res.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+      secure: this.configService.getOrThrow('NODE_ENV') !== 'local',
+      sameSite: 'lax',
+      maxAge: +ms(
+        this.configService.getOrThrow('JWT_REFRESH_TOKEN_EXPIRE_TIME_STRING'),
+      ),
+    });
+
+    return ResponseBuilder.buildSingle(new LoginResponseDto(tokens));
+  }
+
+  @Post('refresh-access-token')
+  @ApiOperation({ summary: 'Get new access token' })
+  @ApiSingleDataResponse(LoginResponseDto)
+  async refresh(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const oldRefreshToken = req.cookies?.refreshToken;
+
+    const tokens = await this.authService.getNewRefreshToken(oldRefreshToken);
+
+    // set refresh token in cookie
+    res.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+      secure: this.configService.getOrThrow('NODE_ENV') !== 'local',
+      sameSite: 'lax',
+      maxAge: +ms(
+        this.configService.getOrThrow('JWT_REFRESH_TOKEN_EXPIRE_TIME_STRING'),
+      ),
+    });
+
+    return ResponseBuilder.buildSingle(new LoginResponseDto(tokens));
+  }
+
+  @Post('logout')
+  @ApiOperation({ summary: 'Logout user' })
+  @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
+  async logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie('refreshToken', {
+      sameSite: 'lax',
+      secure: this.configService.getOrThrow('NODE_ENV') !== 'local',
+      httpOnly: true,
+    });
+
+    return { message: 'Cookie cleared' };
+  }
+}
