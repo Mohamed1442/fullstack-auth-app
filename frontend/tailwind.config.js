7264ceb40b33788ed5099d3874fa271b724f0da9
Mohamed1442 <mohamedmagdy7428@gmail.com>
2025-09-07 03:52:22 +0300

    chore(frontend): removing unneccessary data and adding facicon styling globally

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ffd391",
+        danger: "#EF4444",
+      },
+    },
+  },
+  plugins: [],
+};
