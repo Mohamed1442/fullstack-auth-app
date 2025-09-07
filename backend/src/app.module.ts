3ea82b5c9f8b63136df1e7e9a4fb368dcd35f25e
Mohamed1442 <mohamedmagdy7428@gmail.com>
2025-09-07 03:35:29 +0300

    feat(backend): finalizing the backend implementation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -11,7 +13,14 @@ import { AuthModule } from './auth/auth.module';
       envFilePath: ['.env'],
       isGlobal: true,
     }),
-
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          limit: 10,
+          ttl: 60,
+        },
+      ],
+    }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -19,8 +28,8 @@ import { AuthModule } from './auth/auth.module';
         uri: configService.getOrThrow('MONGODB_URI'),
       }),
     }),
-
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,52 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './global/filters';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+
+  // enabling cookie parser
+  app.use(cookieParser());
+
+  // setting security HTTP headers
+  app.use(helmet());
+
+  // global exception filter(Handle all exceptions thrown in the application)
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
+  // global validation pipe
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  // swagger docs configuration
+  const SWAGGER_PRODUCTION_PREFIX = configService.getOrThrow<string>(
+    'SWAGGER_PRODUCTION_PREFIX',
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Authentication Service')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
+    .addServer('', 'Local Environment')
+    .addServer(SWAGGER_PRODUCTION_PREFIX, 'Production Environment')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  // enabling CORS
+  app.enableCors({
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // starting the server
+  await app.listen(configService.getOrThrow<string>('PORT'));
 }
 
 bootstrap();
