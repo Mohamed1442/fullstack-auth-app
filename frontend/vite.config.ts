3a73f811d4639bd2154377cce77e58a47d3a354e
Mohamed1442 <mohamedmagdy7428@gmail.com>
2025-09-07 04:07:07 +0300

    feat(frontend): adding path aliases for improved import management

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
