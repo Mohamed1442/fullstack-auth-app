8a6d2afa63f8f94d78ab0ed2973e9e1575de257c
Mohamed1442 <mohamedmagdy7428@gmail.com>
2025-09-07 20:02:44 +0300

    feat(frontend): adding home component page with some protected component and route

---
+++ frontend/src/pages/Home/HomePage.tsx
@@ -0,0 +1,30 @@
+import { ERole } from "@api/users/users.types";
+import ProtectedComponent from "@components/Guards/ProtectedComponent";
+import MainButton from "@components/MainButton/MainButton";
+import { EROUTES } from "@constants/routes";
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-secondary flex-col">
+      <h2 className="text-4xl text-center font-bold text-gray-800">
+        Welcome to the application.
+      </h2>
+
+      <div className="flex flex-col gap-4 justify-center mt-32">
+        <MainButton onClick={() => navigate(EROUTES.PROFILE)}>
+          Profile
+        </MainButton>
+        <ProtectedComponent allowerdRoles={[ERole.ADMIN]}>
+          <MainButton onClick={() => navigate(EROUTES.USERS)}>
+            View System Users
+          </MainButton>
+        </ProtectedComponent>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
