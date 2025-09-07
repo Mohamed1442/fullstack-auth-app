9f7ff94799afb0b52beb4a7158b3a644d839d44c
Mohamed1442 <mohamedmagdy7428@gmail.com>
2025-09-07 20:04:34 +0300

    refactor(frontend): adding react query and react router configuration for app wide

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./routers";
+
 function App() {
-  return <h1>Hell There</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
